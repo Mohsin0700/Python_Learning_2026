e69abfef2825f3c12f7b0ce9ed92f3d3ef31095c
= <muhammad786mohsin024@gmail.com>
2026-05-28 06:10:45 +0500

    learning string methods

---
+++ data types/data_type_string.py
@@ -0,0 +1,19 @@
+# name = "mohsin"
+# print(name) # mohsin
+# name2 = name.upper() 
+# print(name2) # MOHSIN
+# name3 = name.lower()
+# print(name3) # mohsin
+# name4 = name.capitalize()
+# print(name4) # Mohsin
+
+name = 'mOhSin'
+print(name)
+print(name.capitalize())
+
+name2 = 'aRbAb'
+print(name2, name2.title())
+
+name3 = 'moHSIN'
+print(name3, name3.swapcase())
+
